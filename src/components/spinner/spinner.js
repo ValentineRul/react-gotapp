253c6927c29c1a5ff67e3b9fd726ef012872f7ec
Valentine <psp32@ukr.net>
2021-07-19 15:24:51 +0300

    GOT1

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "./spinner.css";
+
+const Spinner = () => {
+    return (
+        <div class="loadingio-spinner-spin-1ddt03yv80cj">
+            <div class="ldio-mp8ofmps9ys">
+                <div>
+                    <div />
+                </div>
+                <div>
+                    <div />
+                </div>
+                <div>
+                    <div />
+                </div>
+                <div>
+                    <div />
+                </div>
+                <div>
+                    <div />
+                </div>
+                <div>
+                    <div />
+                </div>
+                <div>
+                    <div />
+                </div>
+                <div>
+                    <div />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Spinner;
